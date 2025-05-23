4656302ef18d181fcc14bb4e37bc15ea9f1df862
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-05-23 13:24:00 -0300

    Criado componente Card | Alterando o componente Inicio para se adequar aos Cards

--- src/app/components/card-info/card-info.component.ts
+++ src/app/components/card-info/card-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-info',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-info.component.scss']
 })
 export class CardInfoComponent {
-
+  @Input() tituloCard: string = '';
+  @Input() descCard: string = '';
 }
